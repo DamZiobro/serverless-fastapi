8901f643ef4c913aa1f4b845d0397278dd7761ba
Damian Ziobro <damian@xmementoit.com>
2021-11-17 16:10:39 +0000

    Update version

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 
 def test_version():
     """Testing API version."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "1.0.1"
 
 
 def test_get_hello_returns_200():
@@ -23,4 +23,4 @@ def test_root_hello_returns_200():
     """GET / returns 200."""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "API version: 0.1.0"}
+    assert response.json() == {"message": "API version: 1.0.1"}
